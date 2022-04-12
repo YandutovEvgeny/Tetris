2348b3f155d1b70b57a6e722f0cfcea36574ca4b
yandutka@mail.ru <yandutka@mail.ru>
2022-04-12 21:26:42 +0300

    Add project files.

---
+++ Tetris/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Figure
+    {
+        public int[,,] Array { get; private set; }
+        public int N { get; private set; }
+        public int Count { get; private set; }
+
+        public Figure(int[,,] array, int n, int count)
+        {
+            Array = array;  //Фигура
+            N = n;          //Ширина и высота
+            Count = count;  //Глубина
+        }
+    }
+}
---
+++ Tetris/FigureFromFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Tetris
+{
+    class FigureFromFile : IFigureCreator
+    {
+        public Figure CreateFigure(string filePath)
+        {
+            FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate);
+            StreamReader reader = new StreamReader(fileStream);
+            
+            int n = Convert.ToInt32(reader.ReadLine());
+            int count = Convert.ToInt32(reader.ReadLine()); //ReadLine - читает строку
+            
+            int[,,] buffer = new int[n, n, count];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    buffer[i, j, 0] = reader.Read();    //Read - читает символ
+        }
+
+        public static int[,] Rotate(int[,] m, int n)    //Метод поворотa фигуры
+        {
+            int[,] result = new int[n,n];
+            
+            for (int i = 0; i < n; i++) 
+                for (int j = 0; j < n; j++)
+                    result[i, j] = m[n - j - 1, i]; 
+
+            return result;
+        }
+    }
+}
